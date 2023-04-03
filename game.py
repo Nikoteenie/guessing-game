ab51a0f080235a00f12cf22c23e8a8366c82459c
Mithin Thomas <mithingthomas@gmail.com>
2023-04-03 17:19:38 -0400

    Multiple rounds and high score

--- game.py
+++ game.py
@@ -3,11 +3,12 @@
 import random
 
 TOTAL_GUESSES = 10
+player_guesses = float("inf")
 name = input("What's your name? ")
 print("Let's play a guessing game")
 
 
-def game():
+def game(high_score):
     low = int(input("Select the lowest number in the range: "))
     high = int(input("Select the highest number in the range: "))
 
@@ -17,7 +18,7 @@ def game():
     print("Try to guess my number.")
 
     guess = int(input("Your guess? "))
-    counter = 0
+    counter = 1
     while (random_number != guess):
         counter += 1
         if (counter >= TOTAL_GUESSES):
@@ -26,9 +27,16 @@ def game():
         print("Your guess is too low, try again.") if guess < random_number else print("Your guess is too high, try again.")
         guess = int(input("Your guess? "))
 
-    print(f"Well done, {name}! You found my number in {counter} tries!")
+    if (counter < high_score):
+        high_score = counter
+
+    if (random_number == guess):
+        print(f"Well done, {name}! You found my number in {counter} tries!")
+
+    print(f"Your high score is {high_score} guesses.")
 
     play_again = input("Would you like to play again? ")
-    if (play_again.lower() == "yes"): game()
+    if (play_again.lower() == "yes"): game(high_score)
     else: return
-game()
+
+game(player_guesses)
