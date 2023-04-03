4b8880e1fd6e1ce37f21bd43d5464bc153b451ba
Mithin Thomas <mithingthomas@gmail.com>
2023-04-03 16:24:05 -0400

    ternary operator

--- game.py
+++ game.py
@@ -1,9 +1,6 @@
-#from random import function_name
 
 # Put your code here
 import random
-# or
-# from random import function_name
 
 random_number = random.randint(1, 100)
 
@@ -15,9 +12,7 @@ guess = int(input("Your guess? "))
 counter = 0
 while (random_number != guess):
     counter += 1
-    if (guess < random_number): print("Your guess is too low, try again.")
-    if (guess > random_number): print("Your guess is too high, try again.")
+    print("Your guess is too low, try again.") if guess < random_number else print("Your guess is too high, try again.")
     guess = int(input("Your guess? "))
 
 print(f"Well done, {name}! You found my number in {counter} tries!")
-    # print("Your guess is too low, try again.") if guess < random_number else print("Your guess is too high, try again.")
